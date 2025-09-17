27f2c062023e1a403c46f16990be23403a6f64e2
Haru8644 <haruki_sasuke@icloud.com>
2025-09-18 02:32:25 +0900

    Implement full CRUD for Problems

---
+++ AtCoderRevManager.Api/Dtos/CreateProblemDto.cs
@@ -0,0 +1,10 @@
+﻿namespace AtCoderRevManager.Api.Dtos
+{
+    public class CreateProblemDto
+    {
+        public string Id { get; set; }
+        public string ContestId { get; set; }
+        public string Title { get; set; }
+        public int? Difficulty { get; set; }
+    }
+}
---
+++ AtCoderRevManager.Api/Dtos/UpdateProblemDto.cs
@@ -0,0 +1,8 @@
+﻿namespace AtCoderRevManager.Api.Dtos
+{
+    public class UpdateProblemDto
+    {
+        public string Title { get; set; }
+        public int? Difficulty { get; set; }
+    }
+}
